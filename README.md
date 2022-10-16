9a98428808030fdd576a8d0ce83d832aabe1d02c
Martin R. Hufsky <martin@malamaglo.de>
2022-10-16 18:05:04 +0200

    chore: setup prettier

--- README.md
+++ README.md
@@ -3,47 +3,48 @@
 # Home Assistant Custom Symbols
 
 ## Install
+
 ### HACS (Recommended)
+
 1. **HACS** tab > Menu > **Custom Repository**
-4. Paste this repo's URL
-5. Select **Integration** in the dropdown
-6. Install **custom_symbols** from HACS
-7. Restart Home Assistant
-8. Home Assistant > Settings > Integrations > Add > Custom Symbols > Install
-9. Add your own svg files to folder /config/custom_symbols
+2. Paste this repo's URL
+3. Select **Integration** in the dropdown
+4. Install **custom_symbols** from HACS
+5. Restart Home Assistant
+6. Home Assistant > Settings > Integrations > Add > Custom Symbols > Install
+7. Add your own svg files to folder /config/custom_symbols
 
 The icons should be usable in Home Assistant now. If it doesnt show up, try refreshing the page, private browsing or restart Home Assistant.
 
 ### Manual
+
 1. Copy `custom_components/custom_symbols` into your custom_components folder
 2. Restart Home Assistant
 3. Home Assistant > Settings > Integrations > Add > Custom Symbols > Install
 4. Add your own svg files to folder /config/custom_symbols
 
-  
 The icons should be usable in Home Assistant now. If it doesnt show up, try refreshing the page, private browsing or restart Home Assistant
 
 ## Features
 
- - Supports Home Assistant's icon picker (2021.11.0+)
- - Only one example SVG file included, please use our own files
- - Rendering Modes
-   - Monochrome
-   - Hierarchical **#h**
-   - Palette **#p**
-   - Multicolor **#m**
-   - Variable Color **#h20** , **#20**
-  
+- Supports Home Assistant's icon picker (2021.11.0+)
+- Only one example SVG file included, please use our own files
+- Rendering Modes
+  - Monochrome
+  - Hierarchical **#h**
+  - Palette **#p**
+  - Multicolor **#m**
+  - Variable Color **#h20** , **#20**
 
 ## Usage
- - Prefix: **cs**
- - Suffix: **#[mode[value]]**
- - Eg:
-    - SVG files in config/custom_symbols:
-      - bed.double.svg
-      - ha.svg
-    -   **cs:bed.double** | **cs:ha#h** | **cs:ha#40** | **cs:ha#m40**
 
+- Prefix: **cs**
+- Suffix: **#[mode[value]]**
+- Eg:
+  - SVG files in config/custom_symbols:
+    - bed.double.svg
+    - ha.svg
+  - **cs:bed.double** | **cs:ha#h** | **cs:ha#40** | **cs:ha#m40**
 
 ## Prepare SVG
 
@@ -55,12 +56,13 @@ The easiest way to create svg files is the SF Symbol App and an [prepare script]
 
 Class names define how the element should be rendered.  
 An name has this parts
-```[mode]-[order]:[color]#clear%[break point]```. Everything after order is optional
+`[mode]-[order]:[color]#clear%[break point]`. Everything after order is optional
 
 Mode key words
-  - monochrome
-  - multicolor
-  - hierarchical (used for palette too)
+
+- monochrome
+- multicolor
+- hierarchical (used for palette too)
 
 Break point defines until which value in percent the object should be semi transparent.  
 Clear is only used for variable color rendering to clear the background under the transparent object.
@@ -68,7 +70,8 @@ Clear is only used for variable color rendering to clear the background under th
 **Attention**: At least the monochrome class is required!
 
 Eg included ha.svg:
-``` xml
+
+```xml
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="2.38 -90 108 108">
   <path class="monochrome-0 multicolor-0:cyan hierarchical-0:tertiary" fill="#55BEF0" d="M20 …"/>
   <path class="monochrome-1#clear%0 multicolor-1:white%0 hierarchical-1:primary%0" fill="#FFFFFF" d="M56 …"/>
@@ -81,12 +84,12 @@ Eg included ha.svg:
 
 The colors for palette and multicolor can be overwritten with css variables in your theme
 
-  - cs-primary: currentColor
-  - cs-secondary: var(--primary-color)
-  - cs-tertiary: var(--accent-color)
-  - cs-base: var(--primary-text-color)
-  - and for each multicolor:**name**
-    - cs-**name**: [fill value]
+- cs-primary: currentColor
+- cs-secondary: var(--primary-color)
+- cs-tertiary: var(--accent-color)
+- cs-base: var(--primary-text-color)
+- and for each multicolor:**name**
+  - cs-**name**: [fill value]
 
 ## Security
 
@@ -95,5 +98,4 @@ The class and fill attribute only define the style of a generated path.
 
 ## Limitation
 
- - Does not work together with other icon libs which modify custom icon rendering. Eg. hass-fontawesome
- 
+- Does not work together with other icon libs which modify custom icon rendering. Eg. hass-fontawesome
--- js/main.js
+++ js/main.js
@@ -1,181 +1,190 @@
-const DOMAIN = "custom_symbols"
-const ICON_STORE = {}
+const DOMAIN = "custom_symbols";
+const ICON_STORE = {};
 const CONFIG = {
-  '-': 'order',
-  ':': 'color',
-  '#': 'mode',
-  '%': 'breakPoint',
+  "-": "order",
+  ":": "color",
+  "#": "mode",
+  "%": "breakPoint",
   v: {
-    prefix: 'monochrome',
-    fade: 0.6
+    prefix: "monochrome",
+    fade: 0.6,
   },
   h: {
-    prefix: 'hierarchical',
-    secondary: 'opacity: 0.5',
-    tertiary: 'opacity: 0.2',
-    fade: 0.7
+    prefix: "hierarchical",
+    secondary: "opacity: 0.5",
+    tertiary: "opacity: 0.2",
+    fade: 0.7,
   },
   p: {
-    prefix: 'hierarchical',
-    primary: 'fill: var(--cs-primary, currentColor)',
-    secondary: 'fill: var(--cs-secondary, var(--primary-color, #03a9f4))',
-    tertiary: 'fill: var(--cs-tertiary, var(--accent-color, #ff9800))',
-    fade: 0.6
+    prefix: "hierarchical",
+    primary: "fill: var(--cs-primary, currentColor)",
+    secondary: "fill: var(--cs-secondary, var(--primary-color, #03a9f4))",
+    tertiary: "fill: var(--cs-tertiary, var(--accent-color, #ff9800))",
+    fade: 0.6,
   },
   m: {
-    prefix: 'multicolor',
-    primary: 'fill: var(--cs-primary, currentColor)',
-    secondary: 'fill: var(--cs-base, var(--primary-text-color, #000)); opacity: 0.5',
-    tertiary: 'fill: var(--cs-base, var(--primary-text-color, #000)); opacity: 0.2',
+    prefix: "multicolor",
+    primary: "fill: var(--cs-primary, currentColor)",
+    secondary:
+      "fill: var(--cs-base, var(--primary-text-color, #000)); opacity: 0.5",
+    tertiary:
+      "fill: var(--cs-base, var(--primary-text-color, #000)); opacity: 0.2",
     useFill: true,
-    fade: 0.6
-  }
-}
+    fade: 0.6,
+  },
+};
 
-const compareOrder = (a, b) => a.order > b.order
-const getColor = item => item.color
-const getPath = item => item.path
+const compareOrder = (a, b) => a.order > b.order;
+const getColor = (item) => item.color;
+const getPath = (item) => item.path;
 
-const group = (arr, fn) => arr.reduce((target, value, idx, arr) => {
-  const key = String(fn(value, idx, arr))
-  if (key in target) target[key].push(value)
-  else target[key] = [value]
-  return target
-}, Object.create(null))
+const group = (arr, fn) =>
+  arr.reduce((target, value, idx, arr) => {
+    const key = String(fn(value, idx, arr));
+    if (key in target) target[key].push(value);
+    else target[key] = [value];
+    return target;
+  }, Object.create(null));
 
-const mergeParts = parts => Object.values(group(parts, getColor))
-const combinePaths = paths => paths.map(path => path.getAttribute('d')).join(' ')
-const isMonochrome = path => String(path.classList).includes(CONFIG.v.prefix)
+const mergeParts = (parts) => Object.values(group(parts, getColor));
+const combinePaths = (paths) =>
+  paths.map((path) => path.getAttribute("d")).join(" ");
+const isMonochrome = (path) => String(path.classList).includes(CONFIG.v.prefix);
 
 const getOption = (name, target) =>
-  name.split(/([-:#%])/)
-      .reduce((target, value, idx, arr) => {
-        const key = CONFIG[value]
-        const val = arr[idx + 1]
-        if (key) target[key] = val
-        return target
-      }, target)
+  name.split(/([-:#%])/).reduce((target, value, idx, arr) => {
+    const key = CONFIG[value];
+    const val = arr[idx + 1];
+    if (key) target[key] = val;
+    return target;
+  }, target);
 
-const extractMode = config => path =>
+const extractMode = (config) => (path) =>
   Array.from(path.classList)
-      .filter(v => v.startsWith(config.prefix))
-      .map(name => getOption(name, {path}))
+    .filter((v) => v.startsWith(config.prefix))
+    .map((name) => getOption(name, { path }));
 
-const makeDynamic = value => item => {
+const makeDynamic = (value) => (item) => {
   if (value <= item.breakPoint) {
-    const clear = item.mode == 'clear'
-    const extra = { ...item, color: item.color + 'Fade', fade: clear - 2 }
-    return clear ? [item, extra] : extra
+    const clear = item.mode == "clear";
+    const extra = { ...item, color: item.color + "Fade", fade: clear - 2 };
+    return clear ? [item, extra] : extra;
   }
-  return item
-}
- 
+  return item;
+};
+
 const createStyle = (items, config) => {
-  const last = items[items.length -1]
-  let style = ''
+  const last = items[items.length - 1];
+  let style = "";
 
   if (last.color) {
-    const color = last.color.replace('Fade', '')
-    style += config[color] || (config.useFill && `fill: var(--cs-${color}, ${last.path.getAttribute('fill')})`)
+    const color = last.color.replace("Fade", "");
+    style +=
+      config[color] ||
+      (config.useFill &&
+        `fill: var(--cs-${color}, ${last.path.getAttribute("fill")})`);
   }
 
-  if (last.fade) style += `; opacity: ${Math.abs(1 + last.fade + config.fade)}`
+  if (last.fade) style += `; opacity: ${Math.abs(1 + last.fade + config.fade)}`;
 
-  return style
-}
+  return style;
+};
 
 const preProcessIcon = async (iconSet, iconName) => {
-  const [icon, suffix = ''] = iconName.split("#")
-  const [type, value] = suffix.split(/(\d+)/)
-  const hasValue = value !== undefined
-  const config = CONFIG[type[0]] || (hasValue && CONFIG.v)
-  let viewBox
-  let path = ''
-  let nodes = null
-  
-  const data = await fetch(`/${DOMAIN}/icon/${iconSet}/${icon}.svg`)
-  const text = await data.text()
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(text, 'image/svg+xml')
-  
-  if (!doc || doc.documentElement.nodeName != 'svg') return {}
-  const svg = doc.documentElement
-  const paths = Array.from(svg.querySelectorAll('path'))
+  const [icon, suffix = ""] = iconName.split("#");
+  const [type, value] = suffix.split(/(\d+)/);
+  const hasValue = value !== undefined;
+  const config = CONFIG[type[0]] || (hasValue && CONFIG.v);
+  let viewBox;
+  let path = "";
+  let nodes = null;
 
-  viewBox = svg.getAttribute('viewBox')
+  const data = await fetch(`/${DOMAIN}/icon/${iconSet}/${icon}.svg`);
+  const text = await data.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, "image/svg+xml");
+
+  if (!doc || doc.documentElement.nodeName != "svg") return {};
+  const svg = doc.documentElement;
+  const paths = Array.from(svg.querySelectorAll("path"));
+
+  viewBox = svg.getAttribute("viewBox");
 
   if (config) {
-    let parts = paths.flatMap(extractMode(config)).sort(compareOrder)
+    let parts = paths.flatMap(extractMode(config)).sort(compareOrder);
 
-    if (hasValue) parts = parts.flatMap(makeDynamic(value))
+    if (hasValue) parts = parts.flatMap(makeDynamic(value));
 
-    const length = parts.length
+    const length = parts.length;
 
     if (length == 1 && (config == CONFIG.v || config == CONFIG.h)) {
-      path = parts[0].path.getAttribute('d')
+      path = parts[0].path.getAttribute("d");
     }
 
     if (length && !path) {
-      nodes = mergeParts(parts).map(items => {
-        const node = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-        const style = createStyle(items, config)
-        if (style) node.setAttribute('style', style)
-        node.setAttribute('d', combinePaths(items.map(getPath)))
-        return node
-      })
+      nodes = mergeParts(parts).map((items) => {
+        const node = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "path"
+        );
+        const style = createStyle(items, config);
+        if (style) node.setAttribute("style", style);
+        node.setAttribute("d", combinePaths(items.map(getPath)));
+        return node;
+      });
     }
   }
 
-  if (!path && !nodes) path = combinePaths(paths.filter(isMonochrome)) 
+  if (!path && !nodes) path = combinePaths(paths.filter(isMonochrome));
 
-  return { path, viewBox, nodes }
+  return { path, viewBox, nodes };
 };
 
 const getIcon = async (iconSet, iconName) => {
   const icon = `${iconSet}:${iconName}`;
-  if (!ICON_STORE[icon]) { 
-    ICON_STORE[icon] = preProcessIcon(iconSet, iconName)
+  if (!ICON_STORE[icon]) {
+    ICON_STORE[icon] = preProcessIcon(iconSet, iconName);
   }
-  return ICON_STORE[icon]
+  return ICON_STORE[icon];
 };
 
 const getIconList = async (iconSet) => {
   const data = await fetch(`/${DOMAIN}/list/${iconSet}`);
   const text = await data.text();
   return JSON.parse(text);
+};
+
+if (!("customIcons" in window)) {
+  window.customIcons = {};
 }
 
-if (!('customIcons' in window)) {
-  window.customIcons = {}
-}
+window.customIcons["cs"] = {
+  getIcon: (iconName) => getIcon("cs", iconName),
+  getIconList: () => getIconList("cs"),
+};
 
-window.customIcons['cs'] = {
-  getIcon: (iconName) => getIcon('cs', iconName),
-  getIconList: () => getIconList('cs'),
-}
-
-customElements.whenDefined('ha-icon').then(() => {
-  const HaIcon = customElements.get('ha-icon')
+customElements.whenDefined("ha-icon").then(() => {
+  const HaIcon = customElements.get("ha-icon");
 
   HaIcon.prototype._setCustomPath = async function (promise, requestedIcon) {
-    const icon = await promise
+    const icon = await promise;
 
-    if (requestedIcon !== this.icon) return
+    if (requestedIcon !== this.icon) return;
 
-    this._path = icon.path
-    this._viewBox = icon.viewBox
+    this._path = icon.path;
+    this._viewBox = icon.viewBox;
 
     if (icon.nodes) {
       await this.UpdateComplete;
-      const el = this.shadowRoot.querySelector('ha-svg-icon')
-      if (!el) return
+      const el = this.shadowRoot.querySelector("ha-svg-icon");
+      if (!el) return;
 
-      await el.updateComplete
+      await el.updateComplete;
 
-      const root = el.shadowRoot.querySelector('g')
-      if (!root) return
-      root.innerText = ''
-      icon.nodes.forEach(node => root.appendChild(node.cloneNode(true)))
+      const root = el.shadowRoot.querySelector("g");
+      if (!root) return;
+      root.innerText = "";
+      icon.nodes.forEach((node) => root.appendChild(node.cloneNode(true)));
     }
   };
 });
