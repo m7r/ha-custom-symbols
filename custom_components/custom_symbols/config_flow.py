63c440fc50c618253bee0a35af37934771fd0544
Martin R. Hufsky <martin@malamaglo.de>
2022-10-30 17:11:21 +0100

    feat: use relative path for data dir

--- custom_components/custom_symbols/config_flow.py
+++ custom_components/custom_symbols/config_flow.py
@@ -1,13 +1,17 @@
 from homeassistant import config_entries
 from shutil import copytree
+from os import path
+DIR = path.dirname(path.realpath(__file__))
 
 @config_entries.HANDLERS.register("custom_symbols")
 class CustomSymbolsConfigFlow(config_entries.ConfigFlow):
     async def async_step_user(self, user_input=None):
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
+
         try:
-            copytree('custom_components/custom_symbols/data', 'custom_symbols')
+            copytree(path.join(DIR, 'data'), 'custom_symbols')
         except:
             pass
+
         return self.async_create_entry(title="", data={})
